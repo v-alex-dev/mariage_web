0c9f646b053257316ba9cd84ff0d5dd59ea8f4ed
dev_vens <vensalex1991@gmail.com>
2026-05-02 14:38:03 +0200

    Add Navbar in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Navbar from './components/layout/Navbar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,7 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar></Navbar>
+        {children}
+      </body>
     </html>
   );
 }
