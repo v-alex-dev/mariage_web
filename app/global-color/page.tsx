333d51dfd6786602969c69a2bc9bfbbe97c7291a
dev_vens <vensalex1991@gmail.com>
2026-09-05 17:03:37 +0200

    add import for the page global color

---
+++ app/global-color/page.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { COLOR_SECTIONS, DEFAULT_COLORS, STORAGE_KEY, COUPLE_NAME } from '@/app/lib/colorConfig';
+import type { ColorMap } from '@/app/types/colors';
